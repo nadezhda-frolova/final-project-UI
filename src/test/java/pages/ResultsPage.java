afadb1b4cb1e77e66ae1273dfbc142daf3a46095
Nadya Frolova <n.frolova.kzn@yandex.ru>
2023-04-26 00:07:41 +0300

    final version after review

--- src/test/java/pages/ResultsPage.java
+++ src/test/java/pages/ResultsPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ResultsPage {
